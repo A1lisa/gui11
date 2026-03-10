04305ceea7ed5b2832793f3ce7d15f371cd16ead
A1lisa <aleksandra071078@icloud.com>
2026-03-10 12:46:06 +0800

    код для задачи с сортировкой слов

--- gui11/Form1.Designer.cs
+++ gui11/Form1.Designer.cs
@@ -28,12 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            txtInput = new TextBox();
+            label1 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // txtInput
+            // 
+            txtInput.Location = new Point(12, 12);
+            txtInput.Name = "txtInput";
+            txtInput.Size = new Size(181, 27);
+            txtInput.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(207, 16);
+            label1.Name = "label1";
+            label1.Size = new Size(164, 20);
+            label1.TabIndex = 1;
+            label1.Text = "введите предложение";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 55);
+            button1.Name = "button1";
+            button1.Size = new Size(124, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Сортировать";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(461, 113);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(txtInput);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtInput;
+        private Label label1;
+        private Button button1;
     }
 }
--- gui11/Form1.cs
+++ gui11/Form1.cs
@@ -5,6 +5,51 @@ namespace gui11
         public Form1()
         {
             InitializeComponent();
+            txtInput.Text=Properties.Settings.Default.txtInput;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sentence = txtInput.Text;
+            string[] wordsUb = Logic.splitSentence(sentence);
+            string result = "";
+            
+            for (int i = 0; i < wordsUb.Length; i++ )
+            {
+                result += wordsUb[i] + " ";
+            }
+            MessageBox.Show(result);
+
+            Properties.Settings.Default.txtInput = sentence;
+            Properties.Settings.Default.Save();
+
+        }
+
+
+    }
+
+    public class Logic
+    {
+        public static string[] splitSentence(string sent)
+        {
+            char[] sep = { ' ', '.', ',', '!', '?', ';', ':', '-' };
+            string[] words = sent.Split(sep);
+            for (int i = 0; i < words.Length - 1; i++)
+            {
+                for (int j = 0; j < words.Length - 1 - i; j++)
+                {
+                    if (words[j].Length < words[j + 1].Length)
+                    {
+
+                        string temp = words[j];
+                        words[j] = words[j + 1];
+                        words[j + 1] = temp;
+                    }
+                }
+            }
+            return words;
         }
     }
 }
+
